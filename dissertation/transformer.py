ca5b2b455547b4f25a9a3be260db58d56fbbeffd
joseph.meyer@sap.com <joseph.meyer@sap.com>
2024-05-21 16:24:24 -0700

    refine transformer code

--- dissertation/transformer.py
+++ dissertation/transformer.py
@@ -1,23 +1,61 @@
-
-import os
 import gc
+import os
+
 import numpy as np
+import pandas as pd
 import torch
 from datasets import Dataset
-from transformers import (AutoTokenizer, AutoModelForSequenceClassification, Trainer, TrainingArguments, EarlyStoppingCallback)
+from scipy.stats import pearsonr
 from tqdm import tqdm
+from transformers import (
+    AutoModelForSequenceClassification,
+    AutoTokenizer,
+    EarlyStoppingCallback,
+    Trainer,
+    TrainingArguments,
+)
 
-def prepare_transformer_data(train, valid, test):
-    scales = ['E_Scale_score', 'A_Scale_score', 'O_Scale_score', 'C_Scale_score', 'N_Scale_score']
-    
-    def prepare_data(df):
-        return [df[['Response', scale]].rename(columns={'Response': 'text', scale: 'labels'}) for scale in scales]
-    
-    bert_train_list = prepare_data(train)
-    bert_val_list = prepare_data(valid)
-    bert_test_list = prepare_data(test)
+from dissertation.preprocessing import concatenate_responses, prepare_train_test_data
 
-    return bert_train_list, bert_val_list, bert_test_list
+
+def rename_cols(df, scales):
+    return [
+        df[['Response', scale]].rename(columns={
+            'Response': 'text',
+            scale: 'labels'
+        }) for scale in scales
+    ]
+
+
+def prepare_transformer_data(path):
+
+    # load data and concatenate repsonses
+    df = pd.read_csv(path)
+    df = concatenate_responses(df)
+
+    train, valid, test, y_train_list, y_val_list, y_test_list = prepare_train_test_data(
+        df)
+
+    scales = [
+        'E_Scale_score', 'A_Scale_score', 'O_Scale_score', 'C_Scale_score',
+        'N_Scale_score'
+    ]
+
+    bert_train_list = rename_cols(train, scales)
+    bert_val_list = rename_cols(valid, scales)
+    bert_test_list = rename_cols(test, scales)
+
+    data_dict = {
+        'bert_train_list': bert_train_list,
+        'bert_val_list': bert_val_list,
+        'bert_test_list': bert_test_list,
+        'y_train_list': y_train_list,
+        'y_val_list': y_val_list,
+        'y_test_list': y_test_list
+    }
+
+    return data_dict
+
 
 def transformer(model,
                 tokenizer,
@@ -27,7 +65,6 @@ def transformer(model,
                 max_length=4096,
                 num_layers_to_freeze=0,
                 freeze=False):
-    
     def tokenize_function(examples, tokenizer):
         return tokenizer(examples['text'],
                          padding='max_length',
@@ -77,7 +114,7 @@ def transformer(model,
         batched=True,
         remove_columns=['text'],
     )
-    
+
     valid_dataset = valid_dataset.map(
         lambda examples: tokenize_function(examples, tokenizer),
         batched=True,
@@ -91,25 +128,24 @@ def transformer(model,
     )
 
     # Define Trainer
-    training_args = TrainingArguments(
-        output_dir='./results',
-        do_eval=False,
-        save_total_limit=2,
-        learning_rate=2e-5,
-        gradient_accumulation_steps=4,
-        per_device_train_batch_size=4,
-        per_device_eval_batch_size=8,
-        num_train_epochs=3,
-        fp16=True,
-        logging_dir='./logs')
+    training_args = TrainingArguments(output_dir='./results',
+                                      do_eval=False,
+                                      save_total_limit=2,
+                                      learning_rate=2e-5,
+                                      gradient_accumulation_steps=4,
+                                      per_device_train_batch_size=4,
+                                      per_device_eval_batch_size=8,
+                                      num_train_epochs=6,
+                                      fp16=True,
+                                      logging_dir='./logs')
 
-    trainer = Trainer(model=model,
-                      args=training_args,
-                      train_dataset=train_dataset,
-                      eval_dataset=valid_dataset,
-                      compute_metrics=compute_metrics,
-                      callbacks=[EarlyStoppingCallback(early_stopping_patience=3)]
-)
+    trainer = Trainer(
+        model=model,
+        args=training_args,
+        train_dataset=train_dataset,
+        eval_dataset=valid_dataset,
+        compute_metrics=compute_metrics,
+        callbacks=[EarlyStoppingCallback(early_stopping_patience=3)])
 
     # Train the model
     trainer.train()
@@ -127,20 +163,52 @@ def transformer(model,
     return {'y_pred_test': y_pred_test, 'y_pred_val': y_pred_val}
 
 
-def multi_transformer(train_datasets: list, test_datasets: list, model,
-                      tokenizer):
+def multi_transformer(path: str, model, tokenizer, output_dir):
 
     torch.cuda.empty_cache()
     gc.collect()
+
     results = {}
+    preds = {}
     constructs = ['y_E_val', 'y_A_val', 'y_O_val', 'y_C_val', 'y_N_val']
     counter = 0
-    for train, test in zip(train_datasets, test_datasets):
+
+    # load data
+    data_dict = prepare_transformer_data(path)
+
+    # data
+    train_datasets, valid_datasets, test_datasets = data_dict[
+        'bert_train_list'], data_dict['bert_val_list'], data_dict[
+            'bert_test_list']
+
+    # ys
+    y_test_list = data_dict['y_test_list'],
+
+    for train, valid, test, y_test in zip(train_datasets, valid_datasets,
+                                          test_datasets, y_test_list):
         construct = constructs[counter]
-        y_pred = transformer(model, tokenizer, train, test, construct)
+        run = transformer(model, tokenizer, train, valid, test, construct)
+        y_pred_val = run['y_pred_val']
+        y_pred_test = run['y_pred_val']
+
+        #evaluate performance on test data
+        r = pearsonr(y_test, y_pred_test)[0]
+        print('r: ', r)
+        results[construct] = r
+
+        # store predictions on validation data
+        preds[construct] = y_pred_val
         torch.cuda.empty_cache()
-        results[construct] = y_pred
         counter += 1
+
+    # save predictions across constructs
+    predictions = pd.DataFrame(preds)
+    predictions.to_csv(f'{output_dir}/{model}_transformer_preds.csv')
+    print(f'predictions saved in {output_dir}')
+
+    metrics = pd.DataFrame(results)
+    metrics.to_csv(f'{output_dir}/{model}_transformer_results.csv')
+    print(f'results saved in {output_dir}')
 
     return results
 
